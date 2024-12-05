9279f077d67bc677be649cf8a538408f2cedbce7
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2024-12-05 15:06:44 -0500

    Init ABI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,191 @@ import React, { useState } from "react";
 import { Contract } from "ethers";
 import { JsonRpcProvider } from "ethers/providers";
 
+const _abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_projects",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "_projectId",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "_withdrawalAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "subProjectId",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "token",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "escrow",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "bytes",
+        name: "castHash",
+        type: "bytes",
+      },
+    ],
+    name: "SubProjectCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "tokenAddress",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "caster",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "escrowAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "castHash",
+        type: "bytes",
+      },
+    ],
+    name: "createProject",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "subProjectId",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "projectId",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "projects",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    name: "subProjects",
+    outputs: [
+      {
+        internalType: "address",
+        name: "token",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "escrow",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes32",
+        name: "subProjectId",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "castHash",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "token",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "withdrawTokens",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawalAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+]
+
 const App = () => {
-  // BRETT is our default
   const [address, setAddress] = useState("0x532f27101965dd16442e59d40670faf5ebb142e4");
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const projectContractAddress = process.env.REACT_APP_PROJECT_CONTRACT_ADDRESS;
 
   const fetchTokenData = async () => {
     if (!/^0x[a-fA-F0-9]{40}$/.test(address)) {
@@ -26,6 +206,13 @@ const App = () => {
     ];
 
     try {
+      const testContract = new Contract(projectContractAddress, _abi, provider);
+      console.log(testContract);
+
+    } catch (error) {
+      console.error("Error fetching project data:", error);
+    }
+    try {
       const tokenContract = new Contract(address, ERC20_ABI, provider);
 
       const [symbol, decimals] = await Promise.all([
@@ -38,9 +225,9 @@ const App = () => {
       const response = await fetch(requestString);
       console.log(`response loaded ${JSON.stringify(response)}`);
       const tokenData = await response.json();
-      
       const priceUSD = tokenData.market_data?.current_price?.usd || "N/A";
       const imageUrl = tokenData.image?.small || null;
+
 
       setData({ 
         symbol, 
